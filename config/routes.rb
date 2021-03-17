b8e4e9cc09e73c584a227ac92740994b775b675c
itokanta <80323505+itokanta@users.noreply.github.com>
2021-03-17 15:59:13 +0900

    Revert "Revert "チャットルーム作成機能の実装""
    This reverts commit 4dacae8a949c71b103a42a59bb4d605acf64fe9b.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, onry: [:new, :create]
+  resources :rooms, only: [:new, :create]
 end
